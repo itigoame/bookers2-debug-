f73267394f814db289c0b91923c9f6c7310ac017
EC2 Default User <ec2-user@ip-172-31-25-44.ap-northeast-1.compute.internal>
2022-10-29 07:40:14 +0000

    [add]guest

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
